85477f156827afa017245a036c770f0c5107f0ee
siwar enfaiedh <92674741+siwar234@users.noreply.github.com>
2024-09-01 05:26:47 +0100

    Update ConnectionString.js

--- frontend/src/ConnectionString.js
+++ frontend/src/ConnectionString.js
@@ -1,6 +1,5 @@
-export const url="http:172.10.0.140:8000/api"
+export const url = "http://172.10.0.140:8000/api";
 
-export const httpUrl="http://172.10.0.140:4101"
+export const httpUrl = "http://172.10.0.140:4101";
 
-export const googlepUrl="http://172.10.0.140:8000";
-
+export const googlepUrl = "http://172.10.0.140:8000";
